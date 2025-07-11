9bbf605f79a08613c93ad93f52815fe09068c564
Sakshi Antala <sakshi@saasinnova.com>
2025-07-11 16:14:52 +0530

    env setup

---
+++ app.config.ts
@@ -0,0 +1,33 @@
+import "dotenv/config";
+export default {
+  expo: {
+    name: "Boilerplate",
+    slug: "Boilerplate",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      edgeToEdgeEnabled: true,
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    extra: {
+      apiUrl: process.env.REACT_NATIVE_APP_PUBLIC_URL,
+    },
+  },
+};
